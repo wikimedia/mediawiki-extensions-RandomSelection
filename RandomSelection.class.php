b71084900b2c0a697a87404e7509dcfede98d7e8
Jack Phoenix <jack@countervandalism.net>
2017-07-24 18:04:00 +0300

    Version 2.2.2: RationalWiki changes
    From https://github.com/RationalWiki/mediawiki-extensions-RandomSelection
    
    * Adds {{#choose:}} in addition to the old <choose> tag
    ** Example usage: http://rationalwiki.org/wiki/Fun:Trump_tweet_generator
    * BREAKING CHANGE: Caching in the <choose> tag is disabled only if the user specifies <choose uncached>; previously caching was disabled by default
    
    Change-Id: I857824472237af2352ba5f1bdd01272e000ad964

--- RandomSelection.class.php
+++ RandomSelection.class.php
@@ -6,20 +6,33 @@
  *
  * @file
  * @ingroup Extensions
- * @version 2.2
- * @date 23 June 2015
+ * @version 2.2.2
+ * @date 24 July 2017
  * @author Ross McClure <http://www.mediawiki.org/wiki/User:Algorithm>
  * @link https://www.mediawiki.org/wiki/Extension:RandomSelection Documentation
  */
 class RandomSelection {
 	/**
-	 * Register the <choose> tag with the Parser.
+	 * Register the <choose> tag and {{#choose:option 1|...|option N}} function
+	 * with the Parser.
 	 *
 	 * @param Parser $parser
 	 * @return bool
 	 */
 	public static function register( &$parser ) {
 		$parser->setHook( 'choose', array( __CLASS__, 'render' ) );
+		$parser->setFunctionHook( 'choose', array( __CLASS__, 'renderParserFunction' ), Parser::SFH_OBJECT_ARGS );
+		return true;
+	}
+
+	/**
+	 * Register the magic word ID.
+	 *
+	 * @param array $variableIds
+	 * @return bool
+	 */
+	public static function variableIds( &$variableIds ) {
+		$variableIds[] = 'choose';
 		return true;
 	}
 
@@ -27,12 +40,14 @@ class RandomSelection {
 	 * Callback for register() which actually does all the processing.
 	 *
 	 * @param string $input User-supplied input
-	 * @param array $argv [unused]
+	 * @param array $argv User-supplied arguments to the tag, e.g. <choose uncached>...</choose>
 	 * @param Parser $parser
 	 */
 	public static function render( $input, $argv, $parser ) {
-		# Prevent caching
-		$parser->disableCache();
+		# Prevent caching if specified so by the user
+		if ( isset( $argv['uncached'] ) ) {
+			$parser->disableCache();
+		}
 
 		# Parse the options and calculate total weight
 		$len = preg_match_all(
@@ -88,4 +103,58 @@ class RandomSelection {
 
 		return $parser->recursiveTagParse( $input );
 	}
+
+	/**
+	 * Callback for the {{#choose:}} magic word magic (see register() in this file)
+	 *
+	 * @param Parser $parser
+	 * @param PPFrame $frame
+	 * @param array $args User-supplied arguments
+	 */
+	public static function renderParserFunction( &$parser, $frame, $args ) {
+		$options = array();
+		$r = 0;
+
+		// First one is not an object
+		$arg = array_shift( $args );
+		$parts = explode( '=', $arg, 2 );
+		if ( count( $parts ) == 2 ) {
+			$options[] = array( intval( trim( $parts[0] ) ), $parts[1] );
+			$r += intval( trim( $parts[0] ) );
+		} elseif ( count( $parts ) == 1 ) {
+			$options[] = array( 1, $parts[0] );
+			$r += 1;
+		}
+
+		foreach ( $args as $arg ) {
+			$bits = $arg->splitArg();
+			$nameNode = $bits['name'];
+			$index = $bits['index'];
+			$valueNode = $bits['value'];
+			if ( $index === '' ) {
+				$name = trim( $frame->expand( $nameNode ) );
+				$options[] = array( intval( $name ), $valueNode );
+				$r += intval( $name );
+			} else {
+				$options[] = array( 1, $valueNode );
+				$r += 1;
+			}
+		}
+
+		# Choose an option at random
+		if ( $r <= 0 ) {
+			return '';
+		}
+		$r = mt_rand( 1, $r );
+		for ( $i = 0; $i < count( $options ); $i++ ) {
+			$r -= $options[$i][0];
+			if ( $r <= 0 ) {
+				$output = $options[$i][1];
+				break;
+			}
+		}
+
+		return trim( $frame->expand( $output ) );
+	}
+
 }
